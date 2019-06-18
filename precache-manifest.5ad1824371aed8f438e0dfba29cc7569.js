5cb1b75e43153bee0d26638c0bd805288b1876c7
Soomin Kimu <soominkimu@gmail.com>
2019-06-18 17:23:52 +0900

    SHIHOSHI Home

---
+++ precache-manifest.5ad1824371aed8f438e0dfba29cc7569.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55accf9aaa985fdb66114965cea771bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "faf57e1e3174726fef43",
+    "url": "/static/css/main.3e5176c1.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "faf57e1e3174726fef43",
+    "url": "/static/js/main.b4f45dd2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
